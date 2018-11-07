0c5d2f1920f084bab3bee1c5d462a14e1f3e69cf
Zeztron <pateltharsh@gmail.com>
2018-11-07 10:44:19 -0500

    Created modules

---
+++ app.js
@@ -0,0 +1,17 @@
+var budgetController = (function () {
+
+    // Some code
+
+})();
+
+var UIController = (function() {
+
+    // Some code
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+    // Some code
+
+})(budgetController, UIController);
