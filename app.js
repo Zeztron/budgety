9d7e97794c31a2c2eea33054dcc7f62e1fd4bede
Zeztron <pateltharsh@gmail.com>
2018-11-07 15:19:08 -0500

    Updated UI control to display updated budget

--- app.js
+++ app.js
@@ -93,7 +93,12 @@ var UIController = (function() {
         inputValue: ".add__value",
         inputBtn: ".add__btn",
         incomeContainer: ".income__list",
-        expensesContainer: ".expenses__list"
+        expensesContainer: ".expenses__list",
+        budgetLabel: ".budget__value",
+        incomeLabel: ".budget__income--value",
+        expensesLabel: ".budget__expenses--value",
+        percentageLabel: ".budget__expenses--percentage"
+
     }
     return {
         getInput: function() {
@@ -138,6 +143,18 @@ var UIController = (function() {
             // Focus on description
             fieldsArr[0].focus();
         },
+
+        displayBudget: function(obj) {
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+            if(obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + "%";
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = "---";
+            }
+        },
         // Expose the private DOMstrings to make it public
         getDOMstrings: function() {
             return DOMstrings;
@@ -163,7 +180,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         //2. return the budget
         var budget = budgetCtrl.getBudget();
         //3. Display budget.
-        console.log(budget);
+        UICtrl.displayBudget(budget);
     };
 
     var ctrlAddItem = function() {
@@ -186,6 +203,12 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log("Application has started");
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
             setupEventListeners();
         }
     };
